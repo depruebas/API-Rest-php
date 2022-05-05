c794bf1d0e10b5e195be2f79d4b21a58a1ec7733
depruebas <alex.arenols@depruebas.com>
2022-05-05 20:34:10 +0200

    changes api

--- config/config.php
+++ config/config.php
@@ -1,30 +1,30 @@
 <?php
 
-  return array(
+  return [
 
-    'database' => array(
+    'database' => [
 
-      'cars' => array(
+      'cars' => [
         'dsn' => 'mysql:host=localhost;dbname=API_CARS;charset=utf8mb4',
         'username' => 'root',
         'password' => 'mysql',
-      ),
+      ],
 
-    ),
+    ],
 
     # ruta de logs  de la aplicacion
-    'ruta_logs' => array(
+    'ruta_logs' => [
       'general' =>   dirname( dirname(__FILE__)) . '/logs/',
       'error_log' =>  dirname( dirname(__FILE__)). '/logs/',
-    ),
+    ],
 
     # Verbos rest que acepta la API
-    'methods' => array( 'GET', 'POST', 'PUT', 'DELETE'),
+    'methods' => [ 'GET', 'POST', 'PUT', 'DELETE'],
 
     # Keys necesarias para validar la API y el usuario que  hace la petición
     # En lugar de poner usuario y password podemos poner token y token_secret si en lugar
     # de usuarios estamos trabajando con dispositivos IoT
-    'keys' => array( 'api_key', 'api_key_secret', 'username', 'password', 'api_name', 'access_token' ),
+    'keys' => [ 'api_key', 'api_key_secret', 'username', 'password', 'api_name', 'access_token' ],
 
     # 0 - no depuración / 1 - depuración
     'debug' => 1,
@@ -37,4 +37,4 @@
     'api_enabled' => 1,
     'api_salt' => 'WQ5+VEy&*m&6qw12Ra!',
 
-  );
+  ];
